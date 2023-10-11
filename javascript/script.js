2ba506c9e1de291d0d31efc975592cbe70eea856
iaio998 <depalatisiacopo@gmail.com>
2023-10-11 16:23:30 +0200

    Added fizz, buzz and fizzbuzz on grill

--- javascript/script.js
+++ javascript/script.js
@@ -2,22 +2,30 @@ const wrapperEl = document.getElementById("boxwrapper");
 
 for (let i = 1; i < 101; i++) {
   let bgBox;
+  const boxEl = document.createElement("div");
+  wrapperEl.append(boxEl);
+  boxEl.classList.add(
+    "box",
+    "border",
+    "d-flex",
+    "justify-content-center",
+    "align-items-center"
+  );
 
   if (i % 3 === 0 && i % 5 === 0) {
-    const fizzbuzz = "Fizzbuzz";
-    console.log(fizzbuzz);
+    const fizzBuzz = "FizzBuzz";
+    console.log(fizzBuzz);
+    boxEl.innerHTML = fizzBuzz;
   } else if (i % 3 === 0) {
     const fizz = "Fizz";
     console.log(fizz);
+    boxEl.innerHTML = fizz;
   } else if (i % 5 === 0) {
     const buzz = "Buzz";
     console.log(buzz);
+    boxEl.innerHTML = buzz;
   } else {
     console.log(i);
+    boxEl.innerHTML = i;
   }
-  const boxEl = document.createElement("div");
-  wrapperEl.append(boxEl);
-  wrapperEl.classList.add("d-flex", "flex-wrap");
-  boxEl.classList.add("box", "border");
-  boxEl.innerHTML = i;
 }
