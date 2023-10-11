eefd549f12b3e8bdb8ff349e7ff6379c18633784
iaio998 <depalatisiacopo@gmail.com>
2023-10-11 16:03:45 +0200

    Added grill on screen

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,8 @@
 const wrapperEl = document.getElementById("boxwrapper");
+
 for (let i = 1; i < 101; i++) {
+  let bgBox;
+
   if (i % 3 === 0 && i % 5 === 0) {
     const fizzbuzz = "Fizzbuzz";
     console.log(fizzbuzz);
@@ -12,4 +15,9 @@ for (let i = 1; i < 101; i++) {
   } else {
     console.log(i);
   }
+  const boxEl = document.createElement("div");
+  wrapperEl.append(boxEl);
+  wrapperEl.classList.add("d-flex", "flex-wrap");
+  boxEl.classList.add("box", "border");
+  boxEl.innerHTML = i;
 }
