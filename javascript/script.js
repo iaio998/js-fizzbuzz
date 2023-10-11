5af3fb45904533934901c7a63bfe463eeebc0485
iaio998 <depalatisiacopo@gmail.com>
2023-10-11 16:26:01 +0200

    Added background on boxes

--- javascript/script.js
+++ javascript/script.js
@@ -9,21 +9,25 @@ for (let i = 1; i < 101; i++) {
     "border",
     "d-flex",
     "justify-content-center",
-    "align-items-center"
+    "align-items-center",
+    "bg-success"
   );
 
   if (i % 3 === 0 && i % 5 === 0) {
     const fizzBuzz = "FizzBuzz";
     console.log(fizzBuzz);
     boxEl.innerHTML = fizzBuzz;
+    boxEl.classList.add("bg-info");
   } else if (i % 3 === 0) {
     const fizz = "Fizz";
     console.log(fizz);
     boxEl.innerHTML = fizz;
+    boxEl.classList.add("bg-danger");
   } else if (i % 5 === 0) {
     const buzz = "Buzz";
     console.log(buzz);
     boxEl.innerHTML = buzz;
+    boxEl.classList.add("bg-warning");
   } else {
     console.log(i);
     boxEl.innerHTML = i;
